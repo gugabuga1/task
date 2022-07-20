21daddd065904b0e2430be41fa32ebb59722e7b2
Pavel <liventsouu@gmail.com>
2022-07-20 09:46:54 +0300

    T006

---
+++ task006/task6.java
@@ -0,0 +1,13 @@
+package task006;
+
+import java.util.Scanner;
+
+public class task6 {
+    public static void main(String[] args) {
+        String input;
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("1 2 4 8 16 32 64 128 256 512");
+            input = sc.nextLine();
+        }
+    }
+}
