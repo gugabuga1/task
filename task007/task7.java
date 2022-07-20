adc2641ff85526a265824a5ce916e0885646f883
Pavel <liventsouu@gmail.com>
2022-07-20 09:51:08 +0300

    T007

---
+++ task007/task7.java
@@ -0,0 +1,15 @@
+package task007;
+
+import java.util.Scanner;
+
+public class task7 {
+    public static void main (String[] args){
+        String input;
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("* * * * *");
+            System.out.println("* * * * *");
+            System.out.println("* * * * *");
+            input = sc.nextLine();
+        }
+    }
+}
