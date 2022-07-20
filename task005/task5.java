7debbc7245fcdec43af6c824547ec286c8d0063e
Pavel <liventsouu@gmail.com>
2022-07-20 09:43:41 +0300

    T005

---
+++ task005/task5.java
@@ -0,0 +1,15 @@
+package task005;
+
+import java.util.Scanner;
+
+public class task5 {
+    public static void main (String[] args){
+        String input;
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("7 14 21 28 35 42 49 56 63 70 77 84 91 98");
+            input = sc.nextLine();
+            
+        }
+
+    }
+}
